8b956d1d9426e3d71fad0f71986a47811eecf785
crocodeev <svin862003@mail.ru>
2020-09-15 14:40:17 +0300

    change structure, complete post message

---
+++ telegramNotification/telegramNotification_body.php
@@ -0,0 +1,29 @@
+<?php
+class telegramNotification {
+    public static function sendNotification( $article, $user, $content, $summary,
+        $isMinor, $isWatch, $section, $flags, $revision, $status, $baseRevId ) {
+
+        $curl = curl_init();
+        $articleTitle = $article->getTitle();
+
+        curl_setopt_array($curl, array(
+                CURLOPT_URL => "http://sound.inplay.space/test/p/",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "POST",
+                CURLOPT_POSTFIELDS =>"{\"articleTitle\":\"$articleTitle \",\"summary\":\"$summary\",\"author\":\"$user\" }",
+                CURLOPT_HTTPHEADER => array(
+                        "Content-Type: application/json"
+                                        ),
+                        ));
+
+        curl_exec($curl);
+        curl_close($curl);
+
+        return true;
+    }
+}
